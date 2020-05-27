5faae4f02ce74ae27aaf9ad34476734866875544
jose <jabelcastellanosjoo@unm.edu>
2020-05-27 12:40:46 -0600

    Remove temporary file.

--- a.hs
+++
@@ -1,7 +0,0 @@
-cartesian_aux :: [[a]] -> [[a]] -> [[a]]
-cartesian_aux aux []     = aux
-cartesian_aux aux (x:xs) = cartesian_aux (concat (map (\x1 -> (map (\x2 -> x1 : x2) aux)) x)) xs
-
-cartesian_aux_2 = cartesian_aux [[]]
-
-cartesian_prod = \x -> cartesian_aux_2 (reverse x)
