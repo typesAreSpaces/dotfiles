627ba7d966e45ded03595eb1990ba03108dd4f40
jose <jabelcastellanosjoo@unm.edu>
2020-06-06 11:56:43 -0600

    Add installation file for a Manjaro machine.

---
+++ .manjaro-machine-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# Dot files setup
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+echo ".dotfiles" >> .gitignore
+git clone --bare git@github.com:typesAreSpaces/dotfiles.git $HOME/.dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+mkdir -p .config-backup && \
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+  xargs -I{} mv {} .config-backup/{}
+config checkout
+# ------------------------------------------------------------------------
+# Vim setup
+## Download vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+## Create undodir directory
+mkdir ~/.vim/undodir
+## Install vim plugins
+vim +PlugInstall +qa
+# ------------------------------------------------------------------------
+# Installing my usual stuff
+installManjaroPackages()
+# ------------------------------------------------------------------------
