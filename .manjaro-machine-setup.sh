a8d3653d649f835f583850a3e42e97c631458b07
jose <jabelcastellanosjoo@unm.edu>
2020-06-07 15:17:38 -0600

    Minor changes to manjaro-machine-setup file.

--- .manjaro-machine-setup.sh
+++ .manjaro-machine-setup.sh
@@ -10,13 +10,16 @@ mkdir -p .config-backup && \
   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
   xargs -I{} mv {} .config-backup/{}
 config checkout
+source .zshrc
 # ------------------------------------------------------------------------
 # Vim setup
+## Install Vim
+sudo pacman -S vim
 ## Download vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ## Create undodir directory
-mkdir ~/.vim/undodir
+mkdir -p ~/.vim/undodir
 ## Install vim plugins
 vim +PlugInstall +qa
 # ------------------------------------------------------------------------
